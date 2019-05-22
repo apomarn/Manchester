a03b3000202208a2c2abd99d440406f69554e472
Anghy <anghyf27@hotmail.com>
2019-05-22 13:33:44 -0400

    formfields component

---
+++ src/Components/signin/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component {
+  state = {
+    formError: false,
+    formSuccess: '',
+    formdata: {
+      email: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'email_input',
+          type: 'email',
+          placeholder: 'Enter your email'
+        },
+        validation: {
+          required: true,
+          email: true
+        },
+        valid: false,
+        validationMessage: ''
+      }
+    }
+  }
+
+  render() {
+    return <div>sign in</div>
+  }
+}
+
+export default SignIn
