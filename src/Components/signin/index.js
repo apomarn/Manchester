68b6c0c79b226e1352f2e1911ffdbf611f11951a
Anghy <anghyf27@hotmail.com>
2019-06-08 09:26:48 -0400

    login Component part 2

--- src/Components/signin/index.js
+++ src/Components/signin/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import FormField from '../ui/formFields'
+import { validate } from '../ui/misc'
+import { firebasePromotions } from '../../firebase'
 
 class SignIn extends Component {
   state = {
@@ -23,8 +26,60 @@ class SignIn extends Component {
     }
   }
 
+  updateForm(element) {
+    const newFormdata = { ...this.state.formdata }
+    const newElement = { ...newFormdata[element.id] }
+
+    newElement.value = element.event.target.value
+
+    let validData = validate(newElement)
+    newElement.valid = validData[0]
+    newElement.validationMessage = validData[1]
+
+    newFormdata[element.id] = newElement
+
+    this.setState({
+      formError: false,
+      formdata: newFormdata
+    })
+  }
+
+  submitForm(event) {
+    event.preventDefault()
+
+    let dataToSubmit = {}
+    let formIsValid = true
+
+    for (let key in this.state.formdata) {
+      dataToSubmit[key] = this.state.formdata[key].value
+      formIsValid = this.state.formdata[key].valid && formIsValid
+    }
+
+    if (formIsValid) {
+    } else {
+      this.setState({
+        formError: true
+      })
+    }
+  }
+
   render() {
-    return <div>sign in</div>
+    return (
+      <div className='container'>
+        <div className='signin_wrapper' style={{ margin: '100px' }}>
+          <form onSubmit={event => this.submitForm(event)}>
+            <h2>Please Login</h2>
+            <FormField id={'email'} formdata={this.state.formdata.email} change={element => this.updateForm(element)} />
+            <FormField
+              id={'password'}
+              formdata={this.state.formdata.password}
+              change={element => this.updateForm(element)}
+            />
+            <button onClick={event => this.submitForm(event)}>Log In</button>
+          </form>
+        </div>
+      </div>
+    )
   }
 }
 
